bc51c0165cfeeb4db8f66862bcb529bcb565aba9
João Fonseca <joao.fonseca@ua.pt>
2025-05-11 19:44:26 +0200

    add generator script

---
+++ G11GEN.py
@@ -0,0 +1,50 @@
+import argparse
+import random
+
+
+def main(N: int, K: int, D: int, A_group_ratio: float, A_centroid_ratio: float, val_min: float, val_max: float, std_dev: float):
+    points = []
+        
+    NA = int(N * A_group_ratio)    # Number of points in group A
+    NB = N - NA                    # Number of points in group B
+    
+    CA = int(K * A_centroid_ratio) # Number of centroids for group A
+    CB = K - CA                    # Number of centroids for group B
+    
+    # Generate K random centroids
+    C = [[random.uniform(val_min, val_max) for _ in range(D)] for _ in range(K)]
+    
+    # Generate points for group A
+    for i in range(NA):
+        centroid = C[i % CA]
+        point = [random.gauss(centroid[d], std_dev) for d in range(D)] + ['A']
+        points += [point]
+    
+    # Generate points for group B
+    for i in range(NB):
+        centroid = C[CA + (i % CB)]
+        point = [random.gauss(centroid[d], std_dev) for d in range(D)] + ['B']
+        points += [point]
+    
+    # Shuffle the points
+    random.shuffle(points)
+    
+    # Print the points
+    for point in points:
+        print(','.join(map(str, point)))
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('N', type=int, help='Number of points')
+    parser.add_argument('K', type=int, help='Number of centroids')
+    # Optional arguments
+    parser.add_argument('--D', type=int, default=2, help='Number of dimensions (default: 2)')
+    parser.add_argument('--A_group_ratio', type=float, default=0.9, help='Group A ratio (default: 0.9)')
+    parser.add_argument('--A_centroid_ratio', type=float, default=0.5, help='Centroid A ratio (default: 0.5)')
+    parser.add_argument('--val_min', type=float, default=-1, help='Minimum value for centroids (default: -1)')
+    parser.add_argument('--val_max', type=float, default=1, help='Maximum value for centroids (default: 1)')
+    parser.add_argument('--std_dev', type=float, default=0.1, help='Standard deviation for Gaussian distribution (default: 0.1)')
+
+    args = parser.parse_args()
+    main(args.N, args.K, args.D, args.A_group_ratio, args.A_centroid_ratio, args.val_min, args.val_max, args.std_dev)
