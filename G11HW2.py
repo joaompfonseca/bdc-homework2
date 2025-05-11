ac403496bc37a555628386090d6a19e2dbf2a4eb
João Fonseca <joao.fonseca@ua.pt>
2025-05-11 15:13:25 +0200

    parallel exectution in each worker

--- G11HW2.py
+++ G11HW2.py
@@ -1,11 +1,12 @@
 import argparse
 import math
 
+from typing import Iterable, Tuple, List
 from pyspark import RDD, SparkConf, SparkContext
 from pyspark.mllib.clustering import KMeans
 
 
-def dist(u: tuple, C: list) -> float:
+def dist(u: Tuple, C: List) -> float:
     min_i, min_d = 0, float('inf')
     for i, c in enumerate(C):
         d = math.dist(u, c)
@@ -14,13 +15,81 @@ def dist(u: tuple, C: list) -> float:
     return min_i, min_d
 
 
-def mean_vector(X: list) -> tuple:
-    n = len(X[0])
-    mean = [0.0] * n
-    for i in range(n):
-        for x in X:
-            mean[i] += x[i]
-    return tuple(m / len(X) for m in mean)
+def partial_compute(pairs: Iterable[Tuple[int, Tuple]]) -> List[Tuple]:
+    data = dict()
+
+    for centroid, point in pairs:
+        D = len(point) - 1 # Dimension of the points
+        
+        if centroid not in data:
+            data[centroid] = [
+                0,         # count_A
+                [0.0] * D, # sum_A
+                0,         # count_B
+                [0.0] * D, # sum_B
+            ]
+        # Update the counts and sums based on the label
+        if point[-1] == 'A':            
+            data[centroid][0] += 1
+            for i in range(D):
+                data[centroid][1][i] += point[i]
+        elif point[-1] == 'B':
+            data[centroid][2] += 1
+            for i in range(D):
+                data[centroid][3][i] += point[i]
+    
+    # Prepare the output
+    partial_results = []
+    for centroid, results in data.items():
+        centroid_results = []
+        for value in results:
+            if type(value) == list:
+                centroid_results.append(tuple(value))
+            else:
+                centroid_results.append(value)
+        partial_results.append((centroid, tuple(centroid_results)))
+        
+    return partial_results # [(centroid, (count_A, sum_A, count_B, sum_B)), ...]
+
+
+def reduce_compute(x: List[Tuple], y: List[Tuple]) -> List[Tuple]:
+    return (
+        x[0] + y[0],                              # count_A
+        tuple(a + b for a, b in zip(x[1], y[1])), # sum_A
+        x[2] + y[2],                              # count_B
+        tuple(a + b for a, b in zip(x[3], y[3]))  # sum_B
+    )
+
+
+def final_compute(x: Tuple) -> Tuple:
+    count_A, sum_A, count_B, sum_B = x
+    mu_A = [v / count_A if count_A > 0 else 0.0 for v in sum_A]
+    mu_B = [v / count_B if count_B > 0 else 0.0 for v in sum_B]
+    ell = math.dist(mu_A, mu_B)
+    return (count_A, tuple(mu_A), count_B, tuple(mu_B), ell)
+
+
+def partial_compute_2(pairs: Iterable[Tuple[int, Tuple]], P_data: dict) -> List[Tuple]:
+    data = dict()
+    
+    for centroid, point in pairs:
+        if centroid not in data:
+            data[centroid] = [
+                0.0, # delta_A
+                0.0  # delta_B
+            ]
+        # Update the deltas based on the label
+        if point[-1] == 'A':
+            data[centroid][0] += math.dist(point[:-1], P_data[centroid][1]) ** 2
+        elif point[-1] == 'B':
+            data[centroid][1] += math.dist(point[:-1], P_data[centroid][3]) ** 2
+    
+    # Prepare the output
+    partial_results = []
+    for centroid, results in data.items():
+        partial_results.append((centroid, tuple(results)))
+        
+    return partial_results # [(centroid, (delta_A, delta_B)), ...]
 
 
 def computeVectorX(fixed_a, fixed_b, alpha, beta, ell, k):
@@ -49,76 +118,60 @@ def computeVectorX(fixed_a, fixed_b, alpha, beta, ell, k):
     return x_dist
 
 
-def CentroidSelection(P: RDD, K: int) -> list[tuple]:
-    fixed_a = 0.0
-    fixed_b = 0.0
+def CentroidSelection(P: RDD, K: int) -> List[Tuple]:
+    fixed_A = 0.0
+    fixed_B = 0.0
     alpha   = [0.0] * K
     beta    = [0.0] * K
-    mu_A    = [0.0] * K
-    mu_B    = [0.0] * K
     ell     = [0.0] * K
     
-    def compute(X: list) -> tuple:
-        count_A = len([x for x in X if x[-1] == 'A'])
-        count_B = len([x for x in X if x[-1] == 'B'])
-        mu_A    = mean_vector([x[:-1] for x in X if x[-1] == 'A'])
-        mu_B    = mean_vector([x[:-1] for x in X if x[-1] == 'B'])
-        ell     = math.dist(mu_A, mu_B)
-        fixed_a_part = sum(math.dist(x[:-1], mu_A) for x in X if x[-1] == 'A')
-        fixed_b_part = sum(math.dist(x[:-1], mu_B) for x in X if x[-1] == 'B')
-        return count_A, count_B, mu_A, mu_B, ell, fixed_a_part, fixed_b_part
+    P_data = (P.mapPartitions( lambda x:    partial_compute(x)   ) # [(centroid, (count_A, sum_A, count_B, sum_B)), ...]
+               .reduceByKey  ( lambda x, y: reduce_compute(x, y) ) # [(centroid, (count_A, sum_A, count_B, sum_B)), ...]
+               .mapValues    ( lambda x:    final_compute(x)     ) # [(centroid, (count_A, mu_A, count_B, mu_B, ell)), ...]
+               .collectAsMap())                                    # {centroid: (count_A, mu_A, count_B, mu_B, ell)}
     
-    P_data = (P.mapValues(lambda X: compute(X)) # [(centroid, (count_A, count_B, mu_A, mu_B, ell, fixed_a_part, fixed_b_part)),...]
-               .sortByKey()
-               .collect())
+    P_data_2 = (P.mapPartitions( lambda x:    partial_compute_2(x, P_data) ) # [(centroid, (delta_A, delta_B)), ...]
+                 .reduceByKey  ( lambda x, y: (x[0] + y[0], x[1] + y[1])   ) # [(centroid, (delta_A, delta_B)), ...]
+                 .collectAsMap())                                            # {centroid: (delta_A, delta_B)}
     
-    NA = sum(P_data[i][1][0] for i in range(K))
-    NB = sum(P_data[i][1][1] for i in range(K))    
+    NA = sum(P_data[i][0] for i in range(K))
+    NB = sum(P_data[i][2] for i in range(K))
     
     for i in range(K):
-                
-        alpha[i] = P_data[i][1][0] / NA if NA > 0 else 0.0
-        beta[i]  = P_data[i][1][1] / NB if NB > 0 else 0.0
+        alpha[i] = P_data[i][0] / NA if NA > 0 else 0.0
+        beta[i]  = P_data[i][2] / NB if NB > 0 else 0.0
+        ell[i] = P_data[i][4]
+        fixed_A += P_data_2[i][0]
+        fixed_B += P_data_2[i][1]
+    fixed_A /= NA if NA > 0 else 1.0
+    fixed_B /= NB if NB > 0 else 1.0
         
-        mu_A[i] = P_data[i][1][2]
-        mu_B[i] = P_data[i][1][3]
-        
-        ell[i] = P_data[i][1][4]
-        
-        fixed_a += P_data[i][1][5]
-        fixed_b += P_data[i][1][6]
-        
-    fixed_a /= NA if NA > 0 else 1.0
-    fixed_b /= NB if NB > 0 else 1.0
-        
-    x = computeVectorX(fixed_a, fixed_b, alpha, beta, ell, K)
+    x = computeVectorX(fixed_A, fixed_B, alpha, beta, ell, K)
     
     C = [] 
     for i in range(K):
-        xi = x[i]
+        mu_A, mu_B = P_data[i][1], P_data[i][3]
         ci = []
-        for j in range(len(mu_A[i])):
-            cj = ((ell[i] - xi) * mu_A[i][j] + xi * mu_B[i][j]) / ell[i] if ell[i] != 0 else mu_A[i][j]
+        for j in range(len(mu_A)):
+            cj = ((ell[i] - x[i]) * mu_A[j] + x[i] * mu_B[j]) / ell[i] if ell[i] != 0 else mu_A[j]
             ci.append(cj)
         C.append(tuple(ci))
-
     return C 
         
 
-def MRFairLloyd(U: RDD, K: int, M: int) -> list[tuple]:
+def MRFairLloyd(U: RDD, K: int, M: int) -> List[Tuple]:
     # Compute initial set of centroids C using KMeans-parallel
     C = KMeans.train(U.map(lambda u: u[:-1]), K, maxIterations=0).clusterCenters
     
     for _ in range(M):        
         # Partition U into K clusters using set of centroids C
-        P = (U.map       (lambda u: (dist(u[:-1], C)[0], u)) # [(centroid, point),...]
-              .groupByKey())                                 # [(centroid, [point,...]),...]
+        P = U.map(lambda u: (dist(u[:-1], C)[0], u)) # [(centroid, point),...]
         # Compute a fair set of centroids C based on point labels
-        C = CentroidSelection(P, K)        
+        C = CentroidSelection(P, K)
     return C
 
 
-def MRComputeFairObjective(U: RDD, C: list) -> float:
+def MRComputeFairObjective(U: RDD, C: List) -> float:
         return (U.map        ( lambda u:    (u[-1], (dist(u[:-1], C)[1] ** 2, 1)) ) # [(label, (distance, count)),...]
                  .reduceByKey( lambda x, y: (x[0] + y[0], x[1] + y[1])            ) # [(label, (sum_distance, sum_count)),...]
                  .map        ( lambda x:    (1 / x[1][1]) * x[1][0]               ) # [objective,...]       
